008982faee7b763beca59dcabb2fd9533ca4700d
morka17 <joshuamorka19@gmail.com>
2026-08-20 01:40:16 +0100

    Dynamic module: registers producer/consumer per service

---
+++ libs/event-contracts/src/index.ts
@@ -0,0 +1,8 @@
+export * from './envelope';
+
+export * from './events/order.events';
+export * from './events/payment.events';
+export * from './events/inventory.events';
+
+export * from './commands/reserve-stock.command';
+export * from './commands/authorize-payment.command';
